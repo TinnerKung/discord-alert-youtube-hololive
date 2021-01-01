b7cf79e2545fc75fb782a7113be835d0684d3795
TinnerKung <45807795+TinnerKung@users.noreply.github.com>
2021-01-01 11:37:53 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "", 
+    channel: "", 
+    messageTemplate: "ประกาศจากบอท Tinner ผู้ติด Hololive, **{author}** ได้อัปโหลดแล้ว **{title}**!\n{url}", 
+    watchInterval: 60000 
+};
